64860a8f516bb2875dfdc9c1165db4b78ce0e101
Oliver Hookins <ohookins@gmail.com>
2011-11-14 15:32:28 +0100

    Make trailing slashes acceptable.

--- sinagios.rb
+++ sinagios.rb
@@ -7,14 +7,14 @@ require 'json'
 require 'nagios'
 
 # List all the downtime scheduled for all hosts and services
-get '/v1/downtime' do
+get '/v1/downtime/?' do
   nagios = Nagios.new
   content_type 'application/json', :charset => 'utf-8'
   nagios.get_all_downtime.to_json
 end
 
 # List all the downtime scheduled for one host and its services
-get '/v1/downtime/:name' do
+get '/v1/downtime/:name/?' do
   nagios = Nagios.new
   content_type 'application/json', :charset => 'utf-8'
   downtime = nagios.get_all_downtime[params[:name]]
@@ -28,7 +28,7 @@ get '/v1/downtime/:name' do
 end
 
 # Delete all downtime scheduled for one host and its services
-delete '/v1/downtime/:name' do
+delete '/v1/downtime/:name/?' do
   nagios = Nagios.new
 
   if nagios.get_all_downtime.has_key?(params[:name])
@@ -41,7 +41,7 @@ end
 # Create downtime for the named machine. It's not really practical to make this
 # an idempotent action so this will always create new downtime. Hey, you can
 # always just delete all downtime for the host before making this call!
-post '/v1/downtime/:name' do
+post '/v1/downtime/:name/?' do
   # Do some basic validation
   # FIXME: Better feedback of problems.
   begin
